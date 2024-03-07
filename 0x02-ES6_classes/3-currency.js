1fdd0fc67a0a7ec6981dd7bd1e33fbe4f8f55419
dimka90 <dimkayilrit@gmail.com>
2024-03-07 15:33:23 +0100

    fix the eslinter complaint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,29 +3,29 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
-// a function that displays a currency name with it code
+
+  // a function that displays a currency name with it code
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
-// Creating a getter class for the currency code
-  get code()
-  {
+
+  // Creating a getter class for the currency code
+  get code() {
     return this._code;
   }
-// Creating a setter class for the currency code
-  set code(newCode)
-  {
+
+  // Creating a setter class for the currency code
+  set code(newCode) {
     this._code = newCode;
   }
 
   // Creating a getter class for the currency name
-  get name()
-  {
+  get name() {
     return this._code;
   }
-// Creating a setter class for the currency code
-  set name(newName)
-  {
+
+  // Creating a setter class for the currency code
+  set name(newName) {
     this._name = newName;
   }
 }

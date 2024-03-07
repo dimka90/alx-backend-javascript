c6f4687fada9a29915ea574c9ddedc0764db410e
dimka90 <dimkayilrit@gmail.com>
2024-03-07 15:31:53 +0100

    added both getter and setter class for the Currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,8 +3,29 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
-
+// a function that displays a currency name with it code
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
+  }
+// Creating a getter class for the currency code
+  get code()
+  {
+    return this._code;
+  }
+// Creating a setter class for the currency code
+  set code(newCode)
+  {
+    this._code = newCode;
+  }
+
+  // Creating a getter class for the currency name
+  get name()
+  {
+    return this._code;
+  }
+// Creating a setter class for the currency code
+  set name(newName)
+  {
+    this._name = newName;
   }
 }

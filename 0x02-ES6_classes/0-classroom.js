ecc3d928b5ab14ac4db3e3d54785fec021980dfe
dimka90 <dimkayilrit@gmail.com>
2024-03-07 10:40:52 +0100

    fix some bug

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default Classroom {
+constructor(maxStudentsSize)
+{
+this._maxStudentsSize = maxStudentsSize
+}
+}
---
+++ 0x02-ES6_classes/0-main.js
@@ -0,0 +1,4 @@
+import Classroom from "./0-classroom.js"
+
+const room = new Classroom(10);
+console.log(room._maxStudentsSize)

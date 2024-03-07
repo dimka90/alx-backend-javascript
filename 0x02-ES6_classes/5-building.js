c8bf7852752bf0e86b522476854e74fc74bcd9bb
dimka90 <dimkayilrit@gmail.com>
2024-03-07 18:02:40 +0100

    fix a bug

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,14 @@
-export default class Building{
-constructor(sqft)
-{
-    if (typeof sqft !== 'number') throw new TypeError("sqft must be a number");
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
+    this._sqft = sqft;
+  }
 
-    this._sqft = sqft
-}
-// setting  a getter class for sqft
-get sqft()
-{
+  // setting  a getter class for sqft
+  get sqft() {
     return this._sqft;
-}
-
-evacuationWarningMessage()
-{
-
-    throw new TypeError('Class extending Building must override evacuationWarningMessage');
-}
+  }
 }

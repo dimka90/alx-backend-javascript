88c25bdafc7a6a979eb75f0525ee1c175a047156
dimka90 <dimkayilrit@gmail.com>
2024-03-13 04:55:26 +0100

    fix some bug

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,13 @@
-export default function cleanSet(set, startString) {
-  let result = '';
-
-  // looping through the values of the set
-
-  for (const element of set) {
-    // checking to see if a there is a match
-
-    if (element.startsWith(startString)) {
-      result += `${element.substring(startString.length)}-`;
-    }
-  }
-  if (result.length > 0) {
-    result = result.slice(0, -1);
-  }
-
-  return result;
-}
+const cleanSet = (set, startString) => {
+    const strings = [];
+  
+    if (startString === '' || typeof startString !== 'string') return '';
+    set.forEach((s) => {
+      if (typeof s === 'string' && s.startsWith(startString)) {
+        strings.push(s.slice(startString.length));
+      }
+    });
+    return strings.join('-');
+  };
+  
+  export default cleanSet;

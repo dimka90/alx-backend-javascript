5ff04321f62fdc84d7542e22d4fcd3b0f148a4bc
dimka90 <dimkayilrit@gmail.com>
2024-06-11 15:04:50 +0100

    fix a bug on the resolve and reject in number 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,15 @@
-export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve(
-        {
-          status: '200',
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
           body: 'Success',
-        },
-      );
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-}
+        });
+      } else {
+        reject(new Error(
+          'The fake API is not working currently',
+        ));
+      }
+    });
+  }
+  

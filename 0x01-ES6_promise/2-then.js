e04f7b73bdaac2dba05b4f1cdd856a65dde2cd51
dimka90 <dimkayilrit@gmail.com>
2024-06-11 16:12:58 +0100

    return Error instead of New Error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,6 +4,6 @@ export default function handleResponseFromAPI(promise) {
       status: 200,
       body: 'Success',
     }))
-    .catch(() => new Error())
+    .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }

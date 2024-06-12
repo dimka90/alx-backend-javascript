aeaecc5fe8952afe6d84e9ec1ad311530e5fd074
dimka90 <dimkayilrit@gmail.com>
2024-06-12 14:02:46 +0100

    use the promise.all method instead of calling all the promises one by one

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise = uploadPhoto();
-  promise
+  return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
-      process.stdout.write(value.body);
-      return createUser();
-    })
-    .then((value) => {
-      console.log(` ${value.firstName} ${value.lastName}`);
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }

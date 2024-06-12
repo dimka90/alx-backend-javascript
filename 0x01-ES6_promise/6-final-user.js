5a8e1724b4c5aea18ac94ad960811bd43a0e144a
dimka90 <dimkayilrit@gmail.com>
2024-06-12 15:07:00 +0100

    fix some more bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,14 @@
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((value) => {
+    .then((value) => (
       value.map((response) => ({
         status: response.status,
         value: response.status === 'fulfilled' ? response.value : String(response.reason),
-      }));
-    });
+      }))
+    ));
 }
